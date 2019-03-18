af3e02970f0384eee7484a746f325e6be448a8f9
Alex <43651369+tunkytime@users.noreply.github.com>
2019-03-18 12:31:10 -0600

    added favorites section

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,17 +14,27 @@
 
 	// Array of topics
 	var topics = ["Dog", "Cat", "Mouse", "Bunny", "Bird"];
+	
+	// Array of favorites
+	var favorites = [];
 
 
 // FUNCTIONS
 // ===============================
-
 	function renderBtns (array) {
 		for (var i = 0; i < array.length; i++) {
 			var newBtn = $("<button>").text(array[i]);
-			newBtn.addClass("animal-btn btn btn-primary m-1").attr("data-name", array[i]);
+			newBtn.addClass("animal-btn btn btn-info m-1").attr("data-name", array[i]);
 			newBtn.appendTo("#button-div");
 		};
+	};
+	
+	function renderFavBtn (ref) {
+		var favoritesBtn = $(`<button class="favorites btn btn-sm btn-success mb-2 ml-2 mr-2">Add to Favorites</button>`);
+		favoritesBtn.attr("data-url", ref.data("url"));
+		favoritesBtn.attr("data-rating", ref.data("rating"));
+		favoritesBtn.attr("data-title", ref.data("title"));
+		ref.parent().append(favoritesBtn);
 	};
 
 	function runQuery (queryURL) {
@@ -35,60 +45,77 @@
 		}).done(function (res) {
 			console.log(queryURL);
 			for (var i = 0; i < res.data.length; i++) {
+				var gifSrc = res.data[i].images.fixed_height.url;
+				var still = res.data[i].images.fixed_height_still.url;
+				var animated = res.data[i].images.fixed_height.url;
+				var gifDiv = $("<div>").addClass("float-left mt-2 mr-2 border border-dark rounded text-center");
 				
-				// Add <div> to hold elements
-				var gifDiv = $("<div>");
-				gifDiv.addClass("float-left mt-2 mr-2 border rounded text-center");
+				// Add <img> to hold gif
+				var gif = $("<img>").attr("src", gifSrc);
+				gif.addClass("gif");
+				gif.attr("data-still", still);
+				gif.attr("data-animate", animated);
+				gif.attr("data-state", "animate");
 				
 				// Add download link
-				var download= $("<a>");
-				download.attr("href", res.data[i].images.original.url);
-				console.log(res.data[i].url);
+				var download = $("<a>");
+				var downloadBtn = $("<button>").text("Download");
+				downloadBtn.addClass("btn btn-sm btn-secondary mb-2 mr-2 ml-2");
+				download.attr("href", gifSrc);
 				download.attr("download", true);
 				download.attr("target", "_blank");
-				var downloadBtn = (`<button class="btn btn-sm btn-secondary mb-2 mr-2 ml-2">Download</button>`);
 				download.html(downloadBtn);
 				
 				// Add favorites button
-				var favorites = $(`<button class="favorites btn btn-sm btn-primary mb-2 ml-2 mr-2">Add to Favorites</button>`);
-				favorites.attr("data-url", res.data[i].images.fixed_height.url);
-
-				// Add <img> to hold gif
-				var gif = $("<img>").attr("src", res.data[i].images.fixed_height.url);
-				gif.attr("data-still", res.data[i].images.fixed_height_still.url);
-				gif.attr("data-animate", res.data[i].images.fixed_height.url);
-				gif.attr("data-state", "animate");
-				gif.addClass("gif");
+				var favoritesBtn = $(`<button class="favorites btn btn-sm btn-success mb-2 ml-2 mr-2">Add to Favorites</button>`);
+				favoritesBtn.attr("data-url", gifSrc);
+				favoritesBtn.attr("data-rating", res.data[i].rating);
+				favoritesBtn.attr("data-title", res.data[i].title);
+				favoritesBtn.attr("data-still", still);
+				favoritesBtn.attr("data-animate", animated);
+				favoritesBtn.attr("data-state", "animate");
 				
-				// Add <p> to hold rating
 				var rating = $("<p>").text(`Rating: ${res.data[i].rating}`);
-				
-				// Add <p> to hold title
-				var title = $(`<p class="m-1"><strong>${res.data[i].title}</strong></p>`);
-				
-				// Append elements
+				var title = $(`<p class="m-1"><strong>${res.data[i].title}</strong></p>`);	
 				$("#get-more").html(`<span>Get More Gifs!</span>`)
-				gifDiv.append(title);
-				gifDiv.append(gif);
-				gifDiv.append(rating);
-				gifDiv.append(download);
-				gifDiv.append(favorites);
+				
+				gifDiv.append(title, gif, rating, download, favoritesBtn);
 				$("#giphy-div").append(gifDiv);
 			};
 		});
 	};
+	
+	function addGifToFav(ref) {
+		$("#show-favs").css("display", "block");
+		var index = favorites.indexOf(ref.data("url"));
+		ref.parent().remove();
+			
+		if (index > -1) {
+			return false;
+		} else {
+			favorites.push(ref.data("url"));
+			var gifDiv = $("<div>").addClass("float-left mt-2 mr-2 border rounded text-center");
+			var gif = $("<img>").attr("src", ref.data("url")).addClass("gif");
+			gif.attr("data-still", ref.data("still"));
+			gif.attr("data-animate", ref.data("animate"));
+			var rating = $("<p>").text(`Rating: ${ref.data("rating")}`);
+			var title = $(`<p class="m-1"><strong>${ref.data("title")}</strong></p>`);
+			var downloadBtn = (`<a href="${ref.data("url")}"><button class="btn btn-sm btn-secondary mb-2 mr-2 ml-2" target="_blank" download>Download</button></a>`);
+			var removeBtn = $("<button>").addClass("remove btn btn-sm btn-danger mb-2 mr-2 ml-2").text("Remove");
+			removeBtn.attr("data-url", ref.data("url"));
+			removeBtn.attr("data-rating", ref.data("rating"));
+			removeBtn.attr("data-title", ref.data("title"));
+			gifDiv.append(title, gif, rating, downloadBtn, removeBtn);
+			$("#favorites").append(gifDiv);
+		};
+	};
 
 	function generateURL () {
-		// Clear the div
 		$("#get-more").css("display", "block");
 		$("#giphy-div").empty();
-		
-		// Get query term
 		queryTerm = ($(this).data("name"));
-		
-		// Add the search term and number of results to base URL
 		var newURL = queryURLBase + "&q=" + queryTerm;
-				
+
 		// Send AJAX call the new URL
 		runQuery(newURL);
 	};
@@ -100,22 +127,18 @@
 
 // MAIN PROCESS
 // ===============================
+$(document ).ready(function() {
 
-$( document ).ready(function() {
-
+	// Show current list of topics
 	renderBtns(topics);
-	
-	// $(document).on("click")... for dynamically generated elements
-	
+		
 	// Generate gifs
 	$(document).on("click", ".animal-btn", generateURL);
 		
-	// Pause/animate gifs
+	// Pause and animate gifs
 	$(document).on("click", ".gif", function () {
-		// Get current state
 		var state = $(this).attr("data-state");
 
-		// Update state when clicked
 		if (state === "animate") {
 			$(this).attr("src", $(this).attr("data-still"));
 			$(this).attr("data-state", "still");
@@ -127,15 +150,39 @@ $( document ).ready(function() {
 	
 	// Add gif to favorites
 	$(document).on("click", ".favorites", function () {
-		console.log($(this).data("url"));
+		addGifToFav($(this));
+		console.log(this);
+	});
+	
+	// Show favorites
+	$(document).on("click", "#show-favs", function () {
+		$("#hidden-favs").css("display", "block");
+		$("#main-gif-display").css("display", "none");
+		$(this).css("display", "none");
+		var backBtn = $("<button>").addClass("go-back btn btn-dark").text("Go Back");
+		$("body").prepend(backBtn);
+	});
+	
+	// Remove gif from favorites
+	$(document).on("click", ".remove", function () {	
+		favorites.splice($(this).data("url"));
+		renderFavBtn($(this));
+		$("#giphy-div").prepend($(this).parent());
+		$(this).remove();
+	});
+	
+	// Back to main page
+	$(document).on("click", ".go-back", function () {
+		$("#main-gif-display").css("display", "block");
+		$("#hidden-favs").css("display", "none");
+		$(this).css("display", "none");
+		$("#show-favs").css("display", "block");
 	});
 	
 	// Get more results
-	$("#get-more").on("click", function () {				
-		// Get the number of results
+	$("#get-more").on("click", function () {	
+		$("#giphy-div").empty();
 		numResults += 10;
-		
-		// Add the search term and number of results to base URL
 		var newURL = queryURLBase + "&q=" + queryTerm + "&limit=" + numResults;
 		
 		// Send AJAX call the new URL
@@ -145,25 +192,12 @@ $( document ).ready(function() {
 	// Grab value from input box and append to #button-div
 	$("#add-item").on("click", function () {
 		event.preventDefault();
-		
 		if ($("#new-item").val() === "") {
 			return false;
-			
 		} else {
-			var newBtn = $("<button>")
-			var btnText = $("#new-item").val();
-			
-			// Convert to all lower case and capitalize first letter
-			btnText = btnText.toLowerCase();
+			var btnText = $("#new-item").val().toLowerCase();
 			btnText = capitalizeFirstLetter(btnText);
-
-			newBtn.text(btnText);
-			newBtn.addClass("animal-btn btn btn-primary m-1").attr("data-name", btnText);
-			newBtn.appendTo("#button-div");
-			
-			topics.push(name);
-			
-			// Clear input box
+			var newBtn = $("<button>").text(btnText).addClass("animal-btn btn btn-info m-1").attr("data-name", btnText).appendTo("#button-div");
 			$("#new-item").val("");
 		};
 	});
